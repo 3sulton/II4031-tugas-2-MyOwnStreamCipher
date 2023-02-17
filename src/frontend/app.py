320895ae1354b13d10728bf22f6f2a3e740f7244
khffnsa52 <18220056@std.stei.itb.ac.id>
2023-02-17 20:00:44 +0700

    Update app.py

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -373,16 +373,6 @@ def App(screen=None):
         fill="#900C00",
         font=("Poppins Regular", 15 * -1)
     )
-
-    # message save
-    canvas.create_text(
-        300.0,
-        540.0,
-        anchor="nw",
-        text="*tombol download hanya untuk file",
-        fill="#900C00",
-        font=("Poppins Regular", 15 * -1)
-    )
     
     image_credit = PhotoImage(
         file=relative_to_assets("credit.png"))
